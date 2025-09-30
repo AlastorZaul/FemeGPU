11da4d801f48b3abe3b798b354ea2694686da1bb
jerome.desvaux <42062531+AlastorZaul@users.noreply.github.com>
2025-09-30 12:03:19 +0200

    modification service

--- src/app/services/gpu-data.service.ts
+++ src/app/services/gpu-data.service.ts
@@ -11,14 +11,13 @@ export class GpuDataService {
   private http = inject(HttpClient);
 
   // Définissez l'URL de base de votre API
-  private apiUrl = 'http://ai-manager-tu1.ia1.metier.devpass.sf.intra.laposte.fr/api'; // J'ai supposé le protocole http et un chemin /api
-
+  private apiUrl = 'https://ai-manager-tu1.ia1.metier.devpass.sf.intra.laposte.fr/api/admin/'; // J'ai supposé le protocole http et un chemin /api
   // Observable qui interroge l'API toutes les 5 secondes (5000ms)
   public gpuData$: Observable<Cluster[]> = timer(0, 5000).pipe(
     // switchMap annule la requête précédente si une nouvelle arrive
     switchMap(() =>
       // On fait un appel GET à l'endpoint qui retourne les clusters
-      this.http.get<Cluster[]>(`${this.apiUrl}/clusters`).pipe(
+      this.http.get<Cluster[]>(`${this.apiUrl}/infra/v2/dashboard/total_ressource_infrastructures`).pipe(
         // En cas d'erreur de connexion à l'API, on retourne un tableau vide pour ne pas casser l'application
         catchError((error: HttpErrorResponse) => {
           console.error('Erreur de connexion à l\'API :', error.message);
